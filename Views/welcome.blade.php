71ded27a29a23c77e2909573e698b31da86749f0
mayhxmm <55783121+mayhxmm@users.noreply.github.com>
2021-01-16 23:46:50 +0800

    Update welcome.blade.php

--- Views/welcome.blade.php
+++ Views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div style="text-align:center" >
-<h1>Welcome to</br>IIUM Student Compound Management System</h1>
+<h1>Welcome to</br>IIUM Compound Management System</h1>
     {{ date('d-m-Y') }} | <span id="datetime"></span>
     <script>
     var dt = new Date();
