86fd59b99af1fdda63e1134600b10b8f376acc5b
mayhxmm <55783121+mayhxmm@users.noreply.github.com>
2021-01-17 14:53:31 +0800

    Update web.php

--- Route/web.php
+++ Route/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-//Redirect to Welcome page 
+//Welcome page for first time 
 Route::get('/', function () {
     return view('welcome'); });
 
@@ -24,8 +24,6 @@ Route::resource('students', 'App\Http\Controllers\StudentsController');
 //Authentication controller
 Auth::routes();
 
-//Extract Record Count and Latest Record from Students Model
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
-Auth::routes();
-
+//Redirect to Home page
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
